ad25e685f306f38340f145fe624adc5df030f1a2
Josh Wiebe <jwiebe@gms.ca>
2020-09-24 13:28:46 -0600

    testing the joining of streams

--- BridgePOC/src/main/java/gms/cims/bridge/StreamJoiner.java
+++ BridgePOC/src/main/java/gms/cims/bridge/StreamJoiner.java
@@ -1,50 +1,25 @@
 package gms.cims.bridge;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.Deserializer;
-import org.apache.kafka.common.serialization.Serde;
+import com.google.gson.JsonObject;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.JoinWindows;
 import org.apache.kafka.streams.kstream.KStream;
-
-import java.time.Duration;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 public class StreamJoiner {
-
-    public Topology buildTopology() {
-        StreamsBuilder builder = new StreamsBuilder();
-
-        final String ESIClaimImportDetailTopic = "CIMSTEST.Financial.ClaimStatus";
-        final String ESIClaimFileRecordTopic = "CIMSTEST.Financial.ClaimStatusClaimLink";
-        final String outputTopic = "outer-join-output-topic";
-
-        KStream<Object, Object> topic1 = builder.stream(ESIClaimImportDetailTopic);
-        KStream<Object, Object> topic2 = builder.stream(ESIClaimFileRecordTopic);
-
-        /*KStream<Object, Object> outerJoined = topic1.outerJoin(
-                topic2,
-                (leftValue, rightValue) -> "left=" + leftValue + ", right=" + rightValue,
-                JoinWindows.of(Duration.ofMinutes(5)));*/
-
-        topic1.to(outputTopic);
-        //topic2.to(outputTopic);
-
-        return builder.build();
-    }
 
     public void Start() {
 
         Topology topology = buildTopology();
         Properties props = new Properties();
 
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "hello_world3");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "hello_world5");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, Arguments.Broker);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, KafkaAvroSerde.class);
@@ -59,5 +34,16 @@ public class StreamJoiner {
             System.exit(1);
         }
         System.exit(0);
+    }
+
+    public Topology buildTopology() {
+        StreamsBuilder builder = new StreamsBuilder();
+        final String outputTopic = "claim-topic";
+
+        Arguments.Topics.forEach(topicName -> {
+            KStream<String, Object> topic = builder.stream(topicName);
+        });
+
+        return builder.build();
     }
 }
